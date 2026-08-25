e130794babcdef38dc00343581ecab9b2eaf6086
Shawn Gordon <shawn.k.gordon@gmail.com>
2026-08-25 19:15:04 -0400

    Publish Endgame three-path proof board

--- js/endgame-20260823.js
+++ js/endgame-20260823.js
@@ -9,7 +9,8 @@
     ['./css/endgame-mermaid-r2.css?v=20260824-r2','mermaid-r2'],
     ['./css/endgame-public-view-r1.css?v=20260824-r1','public-view-r1'],
     ['./css/final-mermaid-oil-routes-r1.css?v=20260825-r1','final-mermaid-oil-routes-r1'],
-    ['./css/mermaid-autofit-r2.css?v=20260825-r2','mermaid-autofit-r2']
+    ['./css/mermaid-autofit-r2.css?v=20260825-r2','mermaid-autofit-r2'],
+    ['./css/endgame-three-angles-20260825-r3.css?v=20260825-r3','three-angles-20260825-r3']
   ];
   const scripts=[
     ['./js/endgame-adjudication-r1.js?v=20260824-r1','controller'],
@@ -18,7 +19,8 @@
     ['./js/endgame-public-view-r1.js?v=20260824-r1','public-view-r1'],
     ['./js/final-mermaid-oil-routes-r1.js?v=20260825-r1-hotfix1','final-mermaid-oil-routes-r1'],
     ['./js/mermaid-autofit-r2.js?v=20260825-r2','mermaid-autofit-r2'],
-    ['./js/endgame-current-20260825-r2.js?v=20260825-r2','current-20260825-r2']
+    ['./js/endgame-current-20260825-r2.js?v=20260825-r3','current-20260825-r2'],
+    ['./js/endgame-three-angles-20260825-r3.js?v=20260825-r3','three-angles-20260825-r3']
   ];
   styles.forEach(([href,key])=>{
     if(document.querySelector(`link[data-endgame-r1="${key}"]`))return;
