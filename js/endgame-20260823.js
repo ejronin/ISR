676daab0aacd38c57bd0ffeeaf4fb7c2298ab709
Shawn Gordon <shawn.k.gordon@gmail.com>
2026-08-26 13:19:21 -0400

    Retire superseded workspace UX mutator

--- js/endgame-20260823.js
+++ js/endgame-20260823.js
@@ -12,8 +12,7 @@
     ['./css/mermaid-autofit-r2.css?v=20260825-r2','mermaid-autofit-r2'],
     ['./css/endgame-three-angles-20260825-r3.css?v=20260825-r3','three-angles-20260825-r3'],
     ['./css/endgame-objective-scoreboard-20260825-r2.css?v=20260825-r2','objective-scoreboard-20260825-r2'],
-    ['./css/endgame-ux-plain-language-r1.css?v=20260825-r1','endgame-ux-plain-language-r1'],
-    ['./css/workspace-ux-plain-language-r1.css?v=20260825-r1','workspace-ux-plain-language-r1']
+    ['./css/endgame-ux-plain-language-r1.css?v=20260825-r1','endgame-ux-plain-language-r1']
   ];
   const scripts=[
     ['./js/endgame-adjudication-r1.js?v=20260824-r1','controller'],
@@ -25,8 +24,7 @@
     ['./js/endgame-current-20260825-r2.js?v=20260825-r3','current-20260825-r2'],
     ['./js/endgame-three-angles-20260825-r3.js?v=20260825-r5','three-angles-20260825-r3'],
     ['./js/endgame-objective-scoreboard-20260825-r2.js?v=20260825-r3','objective-scoreboard-20260825-r2'],
-    ['./js/endgame-ux-plain-language-r1.js?v=20260825-r1','endgame-ux-plain-language-r1'],
-    ['./js/workspace-ux-plain-language-r1.js?v=20260825-r1','workspace-ux-plain-language-r1']
+    ['./js/endgame-ux-plain-language-r1.js?v=20260825-r1','endgame-ux-plain-language-r1']
   ];
   styles.forEach(([href,key])=>{
     if(document.querySelector(`link[data-endgame-r1="${key}"]`))return;
